fe6f97f4ab0c97d1b89e157a39916ea331fbf387
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2023-12-12 15:13:27 +0100

    branche model

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -19,7 +19,6 @@
         <li><a href="<?= $view->path('single', array(12)); ?>">Single</a></li>
         <li><a href="<?= $view->path('single2', array(12, 'dedede')); ?>">Single2</a></li>
         <li><a href="<?= $view->path('about'); ?>">About</a></li>
-
       </ul>
     </nav>
   </header>
