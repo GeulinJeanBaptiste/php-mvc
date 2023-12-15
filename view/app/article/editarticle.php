b5a4629e38a3e765704492cb0a90306fe3430d29
Geulin Jean-Baptiste <jeanbaptiste.geulin@gmail.com>
2023-12-15 22:02:55 +0100

    modif css page ajouter

--- view/app/article/editarticle.php
+++ view/app/article/editarticle.php
@@ -5,12 +5,11 @@
         <?= $formAddEdit->label('title') ?>
         <?= $formAddEdit->input('title', 'text', $articleEdit->title) ?>
         <?= $formAddEdit->error('title') ?>
-
-        <?= $formAddEdit->label('content') ?>
-        <?= $formAddEdit->textarea('content', $articleEdit->content) ?>
-        <?= $formAddEdit->error('content') ?>
-
-
+        <div class="form-group">
+            <?= $formAddEdit->label('content') ?>
+            <?= $formAddEdit->textarea('content', $articleEdit->content) ?>
+            <?= $formAddEdit->error('content') ?>
+        </div>
         <?= $formAddEdit->submit('submitted', 'Modifier cette article') ?>
     </form>
 </section>
@@ -22,12 +21,20 @@
         gap: 0.5rem;
         margin-top: 1rem;
         width: 100%;
-        max-width: 400px;
+        max-width: 600px;
         margin-inline: auto;
         padding: 1rem;
         border: 1px solid #ccc;
         border-radius: 0.5rem;
         background-color: #f5f5f5;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    textarea[name=content] {
+        resize: both;
+        min-width: 400px;
+        min-height: 200px;
+        max-width: 500px;
+        max-height: 200px;
     }
 </style>
